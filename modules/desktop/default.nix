0b7d390d000e5bacc25601bb88704254fec5b33b
Paul Olteanu <p.a.olteanu@gmail.com>
2024-05-05 15:31:21 -0400

    reenable hyprland

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -8,7 +8,7 @@ with lib; let
   cfg = config.modules.desktop;
 in {
   imports = [
-    # ./hyprland.nix
+    ./hyprland.nix
     # ./sway.nix
     ./waybar.nix
   ];
@@ -18,57 +18,57 @@ in {
   };
 
   config = mkIf cfg.enable {
-    # gtk = {
-    #   enable = true;
+    gtk = {
+      enable = true;
 
-    #   theme = {
-    #     name = "Arc-Dark";
-    #     package = pkgs.arc-theme;
-    #   };
+      theme = {
+        name = "Arc-Dark";
+        package = pkgs.arc-theme;
+      };
 
-    #   cursorTheme = {
-    #     name = "Adwaita";
-    #     package = pkgs.gnome.adwaita-icon-theme;
-    #   };
+      cursorTheme = {
+        name = "Adwaita";
+        package = pkgs.gnome.adwaita-icon-theme;
+      };
 
-    #   iconTheme = {
-    #     name = "Papirus-Dark";
-    #     package = pkgs.papirus-icon-theme;
-    #   };
+      iconTheme = {
+        name = "Papirus-Dark";
+        package = pkgs.papirus-icon-theme;
+      };
 
-    #   font = {
-    #     name = "Public Sans";
-    #     size = 12;
-    #     package = pkgs.public-sans;
-    #   };
+      font = {
+        name = "Public Sans";
+        size = 12;
+        package = pkgs.public-sans;
+      };
 
-    #   gtk3.extraConfig = {
-    #     gtk-application-prefer-dark-theme = true;
-    #     gtk-button-images = 1;
-    #     gtk-menu-images = 1;
-    #     gtk-xft-antialias = 1;
-    #     gtk-xft-hinting = 1;
-    #     gtk-xft-hintstyle = "hintfull";
-    #     gtk-xft-rgba = "rgb";
-    #   };
-    # };
+      gtk3.extraConfig = {
+        gtk-application-prefer-dark-theme = true;
+        gtk-button-images = 1;
+        gtk-menu-images = 1;
+        gtk-xft-antialias = 1;
+        gtk-xft-hinting = 1;
+        gtk-xft-hintstyle = "hintfull";
+        gtk-xft-rgba = "rgb";
+      };
+    };
 
-    # fonts.fontconfig.enable = true;
+    fonts.fontconfig.enable = true;
 
-    # services.playerctld.enable = true;
-    # services.udiskie.enable = true;
-    # services.swaync.enable = true;
+    services.playerctld.enable = true;
+    services.udiskie.enable = true;
+    services.swaync.enable = true;
 
-    # xdg.portal.enable = true;
-    # xdg.portal.xdgOpenUsePortal = true;
-    # xdg.portal.configPackages = [
-    #   pkgs.hyprland
-    #   pkgs.xdg-desktop-portal-gtk
-    # ];
-    # xdg.portal.extraPortals = [
-    #   pkgs.xdg-desktop-portal-gtk
-    #   pkgs.xdg-desktop-portal-hyprland
-    # ];
+    xdg.portal.enable = true;
+    xdg.portal.xdgOpenUsePortal = true;
+    xdg.portal.configPackages = [
+      pkgs.hyprland
+      pkgs.xdg-desktop-portal-gtk
+    ];
+    xdg.portal.extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-hyprland
+    ];
 
     home.packages = with pkgs; [
       noto-fonts
@@ -83,29 +83,29 @@ in {
       (nerdfonts.override {fonts = ["JetBrainsMono"];})
 
       # Other ====================================================================
-      # cinnamon.nemo-with-extensions
-      # cinnamon.nemo-fileroller
-      # gnome.file-roller
-      # gnome.dconf-editor
-      # pavucontrol
-      # helvum
-      # wl-clipboard
+      cinnamon.nemo-with-extensions
+      cinnamon.nemo-fileroller
+      gnome.file-roller
+      gnome.dconf-editor
+      pavucontrol
+      helvum
+      wl-clipboard
 
-      # gnome.adwaita-icon-theme
-      # papirus-icon-theme
+      gnome.adwaita-icon-theme
+      papirus-icon-theme
       arc-theme
-      # xdg-desktop-portal-gtk
+      xdg-desktop-portal-gtk
       glib
       gsettings-desktop-schemas
       xdg-utils
 
       # TODO: Configure though home manager. These have config options
-      # waybar
-      # rofi-wayland
+      waybar
+      rofi-wayland
 
       # TODO: Configure through home manager. These don't have config options
       # dunst
-      # hyprpaper
+      hyprpaper
     ];
   };
 }
